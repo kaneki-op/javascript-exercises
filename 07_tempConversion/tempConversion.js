3ab4f796a2c642bdcef95b66dabbe08f7f2b53ab
Prakash <85176794+kaneki-op@users.noreply.github.com>
2024-07-08 23:54:08 +0530

    Created function which converts celsius to fahrenheit and fahrenheit to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num1) {
+  const FAREN = (num1-32)*5/9;
+  return +FAREN.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num2) {
+  const CELC = (num2*9/5)+32;
+  return +CELC.toFixed(1);
 };
 
 // Do not edit below this line
